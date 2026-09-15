0bbe97d429b0c5ad9a32e438aae6d517caacabce
Oxoi <123059851+Oxoi5583@users.noreply.github.com>
2026-09-15 13:34:58 +0800

    Update rebuild.sh

--- rebuild.sh
+++ rebuild.sh
@@ -1,3 +1,5 @@
+python3 -m venv .venv
+source .venv/bin/activate
 python -m pip install -r requirements.txt
 git fetch --all && git reset --hard origin/$(git branch --show-current)
 python build.py --source "./papers" --output "/var/wiki/html/tech_news"
