2b24df6088c477a5853357d351733c59d2303d9a
Oxoi <123059851+Oxoi5583@users.noreply.github.com>
2026-09-15 13:32:51 +0800

    Update rebuild.sh

--- rebuild.sh
+++ rebuild.sh
@@ -1,2 +1,3 @@
+python -m pip install -r requirements.txt
 git fetch --all && git reset --hard origin/$(git branch --show-current)
 python build.py --source "./papers" --output "/var/wiki/html/tech_news"
